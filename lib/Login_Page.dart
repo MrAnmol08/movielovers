2836c1b8c4b5a0489ff781ac27892bf711380f6c
Anmol KC <kcanmol388@gmail.com>
2023-01-25 13:52:29 +0545

    Imported google fonts and connected the login page to main page

---
+++ lib/Login_Page.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  late String _email, _password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 50.0),
+                Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  validator: (input) => !input!.contains('@') ? 'Please enter a valid email' : null,
+                  onSaved: (input) => _email = input!,
+                ),
+                SizedBox(height: 20.0),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  validator: (input) => input!.length < 8 ? 'Your password needs to be at least 8 characters' : null,
+                  obscureText: true,
+                  onSaved: (input) => _password = input!,
+                ),
+                SizedBox(height: 20.0),
+                Container(
+                  width: double.infinity,
+                  child: RaisedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        // Perform login here
+                      }
+                    },
+                    child: Text('Login'),
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Container(
+                  width: double.infinity,
+                  child: FlatButton(
+                    onPressed: () {
+                      // Navigate to forgot password page
+                    },
+                    child: Text('Forgot Password?'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
