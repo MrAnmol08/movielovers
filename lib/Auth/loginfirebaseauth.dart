f448a7bd882299b22aa8b46b9d2d459ecdfebd22
Anmol KC <kcanmol388@gmail.com>
2023-05-04 20:00:26 +0545

    Edit profile completed successfully

--- lib/Auth/loginfirebaseauth.dart
+++ lib/Auth/loginfirebaseauth.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:movielovers/Auth/auth_page.dart';
 import 'package:movielovers/util/navbar.dart';
-import 'package:movielovers/Auth/loginfirebaseauth.dart';
+
 
 
 class MainPage extends StatelessWidget {
